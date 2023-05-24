cfc3bcd9e77cce10aa4f4a18b628d00fd8575d86
woolf1717 <Barstpiczynski@gmail.com>
2023-05-24 10:25:00 +0200

    LoginForm Setup

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,15 +1,33 @@
 ﻿import "../src/app/globals.css";
 
+import React from "react";
+import { useState } from "react";
+
 export default function LoginForm() {
+  const [loginInput, setLoginInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
   return (
     <>
-      <form className="bg-blue-950 mt-9 p-2">
-        <label>Login:</label>
-        <br />
-        <input className="h-4 w-36" type="text" />
-        <label>Password:</label>
-        <br />
-        <input className="h-4 w-36" type="text" name="fname" />
+      <form className="bg-blue-950 mt-9 p-2 flex flex-col content-between text-sm">
+        <label className="py-0.5">Login/Email:</label>
+        <input
+          onChange={(e) => setLoginInput(e.target.value)}
+          className="h-4 w-36 text-black text-xs pl-0.5"
+          type="text"
+        />
+        <label className="py-0.5">Password:</label>
+        <input
+          onChange={(e) => setPasswordInput(e.target.value)}
+          className="h-4 w-36 text-black text-xs pl-0.5"
+          type="password"
+          name="fname"
+        />
+        <input
+          className="border border-amber-50 mt-2 mx-10 text-xs"
+          type="submit"
+          value="Submit"
+        ></input>
       </form>
     </>
   );
