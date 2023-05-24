6168eb072cea815d5f8c6502035a4192269a19dd
woolf1717 <Barstpiczynski@gmail.com>
2023-05-24 16:30:38 +0200

    Login form fetures with localstorage setItem nad getItem

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,42 +1,105 @@
 ﻿import "../src/app/globals.css";
 
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function LoginForm() {
   const [loginInput, setLoginInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
 
-  const handleLoggedIn = () => {
+  const handleLogIn = (e: any) => {
+    e.preventDefault();
     setLoggedIn(true);
   };
-  return (
-    <>
-      <form className="bg-blue-950 mt-9 p-2 flex flex-col content-between text-sm">
-        <label className="py-0.5">Login/Email:</label>
-        <input
-          onChange={(e) => setLoginInput(e.target.value)}
-          className="h-4 w-36 text-black text-xs pl-0.5"
-          type="text"
-          minLength={8}
-        />
-        <label className="py-0.5">Password:</label>
-        <input
-          onChange={(e) => setPasswordInput(e.target.value)}
-          className="h-4 w-36 text-black text-xs pl-0.5"
-          type="password"
-          name="fname"
-          minLength={8}
-        />
-        <button
-          className="border border-amber-50 mt-2 mx-10 text-xs"
-          onClick={handleLoggedIn}
-          type="button"
+
+  const handleLogOut = () => {
+    setLoggedIn(false);
+    setLoginInput("");
+    setPasswordInput("");
+  };
+
+  useEffect(() => {
+    setLoggedIn(JSON.parse(window.localStorage.getItem("loggedIn")!));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("loggedIn", loggedIn.toString());
+  }, [loggedIn]);
+
+  useEffect(() => {
+    setPasswordInput(
+      JSON.stringify(window.localStorage.getItem("passwordInput")!)
+    );
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("passwordInput", passwordInput);
+  }, [passwordInput]);
+
+  useEffect(() => {
+    setLoginInput(JSON.stringify(window.localStorage.getItem("loginInput")!));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("loginInput", loginInput);
+  }, [loginInput]);
+
+  let actualForm;
+
+  if (loggedIn) {
+    actualForm = (
+      <>
+        <div className="bg-blue-950 mt-9 p-2 flex flex-col content-between text-sm w-52 -translate-x-12">
+          <p className="text-center">Hello user, {loginInput}!!!</p>
+
+          <p className="text-center">
+            In case you forgot your password it`s: {passwordInput} ;D
+          </p>
+          <button
+            className="border border-amber-50 mt-2 mx-10 text-xs"
+            onClick={handleLogOut}
+            type="button"
+          >
+            Log out
+          </button>
+        </div>
+      </>
+    );
+  } else {
+    actualForm = (
+      <>
+        <form
+          onSubmit={handleLogIn}
+          className="bg-blue-950 mt-9 p-2 flex flex-col content-between text-sm"
         >
-          Log in
-        </button>
-      </form>
-    </>
-  );
+          <label className="py-0.5">Login/Email:</label>
+          <input
+            onChange={(e) => setLoginInput(e.target.value)}
+            className="h-4 w-36 text-black text-xs pl-0.5"
+            type="text"
+            pattern=".{8,}"
+            required
+          />
+          <label className="py-0.5">Password:</label>
+          <input
+            onChange={(e) => setPasswordInput(e.target.value)}
+            className="h-4 w-36 text-black text-xs pl-0.5"
+            type="password"
+            name="fname"
+            pattern=".{8,}"
+            required
+          />
+          <button
+            className="border border-amber-50 mt-2 mx-10 text-xs"
+            type="submit"
+          >
+            Log in
+          </button>
+        </form>
+      </>
+    );
+  }
+
+  return <>{actualForm}</>;
 }
