d5a22738e9d97dd7322eb3bff1954845c2fb07f6
woolf1717 <Barstpiczynski@gmail.com>
2023-05-26 14:09:07 +0200

    Babel config for fontawsome dynamic import + some style.

---
+++ components/shop/shoptopsale.tsx
@@ -0,0 +1,25 @@
+﻿import "../../src/app/globals.css";
+
+import React from "react";
+import ItemVertical from "./shopcomponents/itemvertical";
+
+import shirt from "../../src/pictures/products/koszula/Przechwytywanie.JPG";
+
+export default function ShopTopSale() {
+  return (
+    <>
+      <div className="bg-neutral-50 px-4 pt-2 overflow-hidden rounded-md">
+        <div className="flex justify-between items-center">
+          <h1 className="uppercase text-lg font-bold ">hot sale</h1>
+          <h2 className=" text-sm font-semibold  text-yellow-400">More</h2>
+        </div>
+        <ItemVertical
+          src={shirt}
+          starsprops={2}
+          name={"Men shirt"}
+          price={500}
+        />
+      </div>
+    </>
+  );
+}
