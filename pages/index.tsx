77e89d743896bed80bcf815b6b08e8cd9c5ccdc5
woolf1717 <Barstpiczynski@gmail.com>
2023-05-31 16:51:43 +0200

    Like above.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,16 +15,19 @@ export default function Home() {
       <ReduxContainer>
         <Menu />
         <Container>
-          <h1 className="text-2xl italic">Projekt CV.</h1>
-          <p className="mb-2">
-            In this project my main goal, is to prepare to work as Junior Front
-            End Developer. To do so im useing Next.js framework, becaouse it is
-            recomended by REACT. Im going to explore posibilities of Next.js,
-            React, Redux and Tailwind, combined together. I would like to
-            prepare site with posibility to log in, responvie store area with
-            cart capabilities, blog page where its possible to add a post when
-            logged as user. When I find a job as Frontend WebDev i will reuse
-            the project to learn conecting with database.
+          <h1 className="align-center text-center text-2xl italic">
+            Projekt CV.
+          </h1>
+          <p className="mb-2 pt-2 text-center">
+            In this project, my main goal is to prepare to work as a Junior
+            Front-End Developer. To do so, I&apos;m using the Next.js framework
+            because it is recommended by React. I am going to explore the
+            possibilities of Next.js, React, Redux, and Tailwind combined
+            together. I would like to prepare a site with the possibility to log
+            in, a responsive store area with cart capabilities, and a blog page
+            where it&apos;s possible to add a post when logged in as a user.
+            When I find a job as a Frontend Web Developer, I will reuse the
+            project to learn connecting with a database.
           </p>
           <Profile />
           <Login />
