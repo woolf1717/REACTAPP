e1eaa7643029eb31191f00a8878367f382451e29
woolf1717 <Barstpiczynski@gmail.com>
2023-06-12 09:21:00 +0200

    See more see less of an article

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -1,23 +1,55 @@
 ﻿import React from "react";
 
+import { useState } from "react";
 import Image from "next/image";
 
-const Post = ({ post: { title, body, imgUrl, author }, index }: any) => {
-  return (
+const PostShort = ({ post: { title, body, imgUrl, author }, index }: any) => {
+  const [postVisiblity, setPostVisiblity] = useState(false);
+
+  const visiblePostShort = (
     <div className="mt-2 overflow-hidden rounded-md bg-neutral-50 p-2">
       <h1 className="heading">{title}</h1>
       <div className="relative h-48 w-full">
         <Image className="image " src={imgUrl} alt="post" fill={true} />
       </div>
-      <p>{body}</p>
+      <p>{body.slice(0, 159)}...</p>
       <div className="info">
+        <br />
         <h4>
           Written by: {author} {index}
         </h4>
-        <div className="pt-px text-end text-xs">Read more...</div>
+        <div
+          className="pt-px text-end text-xs"
+          onClick={() => setPostVisiblity(true)}
+        >
+          Read more...
+        </div>
       </div>
     </div>
   );
+  const visiblePostLong = (
+    <div className="mt-2 overflow-hidden rounded-md bg-neutral-50 p-2">
+      <h1 className="heading">{title}</h1>
+      <div className="relative h-48 w-full">
+        <Image className="image " src={imgUrl} alt="post" fill={true} />
+      </div>
+      <p>{body}...</p>
+      <div className="info">
+        <br />
+        <h4>
+          Written by: {author} {index}
+        </h4>
+        <div
+          className="pt-px text-end text-xs"
+          onClick={() => setPostVisiblity(false)}
+        >
+          See less...
+        </div>
+      </div>
+    </div>
+  );
+
+  return <>{!postVisiblity ? visiblePostShort : visiblePostLong}</>;
 };
 
-export default Post;
+export default PostShort;
