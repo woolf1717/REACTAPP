9eae87e768b4c9f9d354c0d4b1c490a6057a269c
woolf1717 <Barstpiczynski@gmail.com>
2023-06-22 14:21:25 +0200

    Popup of curent product vol.1

--- components/shop/shopcomponents/itemvertical.tsx
+++ components/shop/shopcomponents/itemvertical.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 
 import React from "react";
 
+import { useDispatch } from "react-redux";
+
 import Rateing from "./rateing";
+
+import { flagPopupOn } from "../../redux/reduxFeatures/shopPopup";
 
 export default function ItemVertical({
   src,
@@ -14,27 +18,30 @@ export default function ItemVertical({
   starsprops,
   price,
 }: any) {
+  const dispatch = useDispatch();
   return (
     <>
-      <div className="">
-        <Image
-          src={src}
-          width={75}
-          height={75}
-          quality={75}
-          alt={alt}
-          className="relative  left-1/2 -translate-x-2/4 pt-px"
-        />
-      </div>
-      <div className="text-center text-xxxs font-semibold">{name}</div>
-      <div className="text-xxxs  ">{descr}</div>
-      <div className="flex flex-row justify-center text-xxxs text-yellow-400 ">
-        {starsprops}
-        <Rateing stars={starsprops} />
-      </div>
-      <div className="flex justify-around pt-px">
-        <div className="text-xxs font-semibold text-red-600">{price} PLN</div>
-        <div className="text-xxs opacity-75">Sale</div>
+      {" "}
+      <div onClick={() => dispatch(flagPopupOn(name))}>
+        <div>
+          <Image
+            src={src}
+            width={75}
+            height={75}
+            quality={75}
+            alt={alt}
+            className="relative  left-1/2 -translate-x-2/4 pt-px"
+          />
+        </div>
+        <div className="text-center text-xxxs font-semibold">{name}</div>
+        <div className="flex flex-row justify-center text-xxxs text-yellow-400 ">
+          {starsprops}
+          <Rateing stars={starsprops} />
+        </div>
+        <div className="flex justify-around pt-px">
+          <div className="text-xxs font-semibold text-red-600">{price} PLN</div>
+          <div className="text-xxs opacity-75">Sale</div>
+        </div>{" "}
       </div>
     </>
   );
